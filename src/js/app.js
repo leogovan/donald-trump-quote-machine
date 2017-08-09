e9213c1f9b662cf6ae946abdf87a8935bec3c021
Leofric Govan <leogovan@Leos-MacBook-Pro-2.local>
2017-08-09 21:20:51 +0100

    Sliding drawer function

--- src/js/app.js
+++ src/js/app.js
@@ -30,3 +30,12 @@ $(".twitter-share-button").click(function() {
 	console.log(this);
     $(this).attr("href", 'https://twitter.com/intent/tweet?text=' + quoteTest);
 });
+
+var slider = document.querySelector(".tweet-section");
+var button = document.querySelector(".quote-me");
+
+button.addEventListener("click", function(e) {
+	console.log("I am opening");
+	slider.classList.toggle("open");
+	e.stopPropagation();
+});
